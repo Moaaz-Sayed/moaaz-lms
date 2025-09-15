ffe93e95ac9d6c15d2a4546eefbdc47e0a6e1b99
moaaz_sayed <moaaz4032215@gmail.com>
2025-09-15 03:27:23 +0300

    fix bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: ["./lib/generated/**"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
+  { ignores: ["./lib/generated/**"] },
 ];
 
 export default eslintConfig;
