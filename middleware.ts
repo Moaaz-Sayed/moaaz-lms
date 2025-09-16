6dde73bd6cb139a6cd589748aa549492cfc3c462
moaaz_sayed <moaaz4032215@gmail.com>
2025-09-16 22:56:37 +0300

    debugging

--- middleware.ts
+++ middleware.ts
@@ -1,66 +1,52 @@
-// import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
-// import { env } from "./lib/env";
-// import { NextRequest, NextResponse } from "next/server";
-// import { getSessionCookie } from "better-auth/cookies";
-
-// async function authMiddleware(request: NextRequest) {
-//   const sessionCookie = getSessionCookie(request);
-
-//   if (!sessionCookie) {
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-// export const config = {
-//   // matcher tells Next.js which routes to run the middleware on.
-//   // This runs the middleware on all routes except for static assets.
-//   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
-// };
-// const aj = arcjet({
-//   key: env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
-//   rules: [
-//     detectBot({
-//       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-//       // Block all bots except the following
-//       allow: [
-//         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-//         "CATEGORY:MONITOR", // Uptime monitoring services
-//         "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-//         "STRIPE_WEBHOOK",
-//         // Uncomment to allow these other common bot categories
-//         // See the full list at https://arcjet.com/bot-list
-//       ],
-//     }),
-//   ],
-// });
-// // Pass any existing middleware with the optional existingMiddleware prop
-// export default createMiddleware(aj, async (request: NextRequest) => {
-//   const { pathname } = request.nextUrl;
-
-//   if (pathname.startsWith("/api/webhook/stripe")) {
-//     return NextResponse.next();
-//   }
-
-//   if (request.nextUrl.pathname.startsWith("/admin")) {
-//     return authMiddleware(request);
-//   }
-
-//   return NextResponse.next();
-// });
-
-// middleware.ts
+import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
+import { env } from "./lib/env";
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
+async function authMiddleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
 
-export default function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (pathname.startsWith("/api/webhook/stripe")) {
-    return NextResponse.next();
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
+export const config = {
+  // matcher tells Next.js which routes to run the middleware on.
+  // This runs the middleware on all routes except for static assets.
+  matcher: [
+    "/((?!api/webhook/stripe|_next/static|_next/image|favicon.ico|api/auth).*)",
+  ],
+};
+const aj = arcjet({
+  key: env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+  rules: [
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "CATEGORY:MONITOR", // Uptime monitoring services
+        "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+        "STRIPE_WEBHOOK",
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
+// Pass any existing middleware with the optional existingMiddleware prop
+export default createMiddleware(aj, async (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/api/webhook/stripe")) {
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    return authMiddleware(request);
+  }
+
+  return NextResponse.next();
+});
